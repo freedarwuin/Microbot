6eb2209683ae1c6d8d5c7cf74a096c53d178ca23
Darwuin Pedroza <darwuinjose@gmail.com>
2025-04-13 20:41:27 -0400

    nuevos

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/LT/SummerPie/SummerPiesConfig.java
@@ -0,0 +1,8 @@
+package net.runelite.client.plugins.microbot.LT.SummerPie;
+
+import net.runelite.client.config.Config;
+import net.runelite.client.config.ConfigGroup;
+
+@ConfigGroup("SummerPies")
+public interface SummerPiesConfig extends Config {
+}
