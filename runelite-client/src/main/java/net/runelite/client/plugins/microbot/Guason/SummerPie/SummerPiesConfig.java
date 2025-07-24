e37bc7177bc3f42bd667d1c44b3ef01e0a599f8c
Guason <ingdarwuin@gmail.com>
2025-07-24 13:51:01 -0400

    Update README.md

--- runelite-client/src/main/java/net/runelite/client/plugins/microbot/Guason/SummerPie/SummerPiesConfig.java
+++
@@ -1,8 +0,0 @@
-package net.runelite.client.plugins.microbot.Guason.SummerPie;
-
-import net.runelite.client.config.Config;
-import net.runelite.client.config.ConfigGroup;
-
-@ConfigGroup("SummerPies")
-public interface SummerPiesConfig extends Config {
-}
