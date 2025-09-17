c8e6b0a8d34b410a99ec101e450d2086ea704835
ElGuason <ingdarwuin@gmail.com>
2025-09-16 21:50:44 -0400

    Primer Commit

---
+++ runelite-client/src/main/java/net/runelite/client/plugins/microbot/Bones/BonesPlugin.java
@@ -0,0 +1,28 @@
+package net.runelite.client.plugins.microbot.Bones;
+
+import net.runelite.client.plugins.Plugin;
+import net.runelite.client.plugins.PluginDescriptor;
+
+import javax.inject.Inject;
+
+@PluginDescriptor(
+        name = PluginDescriptor.LT + "Bones Burier",
+        description = "Entierra Big Bones automáticamente cerca de un banco",
+        tags = {"bones", "bury", "prayer"},
+        enabledByDefault = false
+)
+public class BonesPlugin extends Plugin {
+
+    @Inject
+    private BonesScript script;
+
+    @Override
+    protected void startUp() {
+        script.run();
+    }
+
+    @Override
+    protected void shutDown() {
+        script.shutdown();
+    }
+}
